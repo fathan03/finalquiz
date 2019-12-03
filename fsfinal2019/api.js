46bedf7a50846a1d10f5c5f4c63c5fcdf18ed338
fathan <s6030213033@phuket.psu.ac.th>
2019-12-03 18:57:11 +0700

    #6 finish

--- fsfinal2019/api.js
+++ fsfinal2019/api.js
@@ -19,8 +19,7 @@ function getAllProducts(req, res) {
 function getProductById(req, res) {
     var pid = req.params.pid;    
     // #6 Get a product by ID
-    var id = req.param.id;
-    Product.find({ "_id": id }, function (err, products) {
+    Product.find({ "_pid": pid }, function (err, products) {
         if (err) res.status(500).json(err);
         res.json(products);
     });
